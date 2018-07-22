c185a59fc3a3b140941fe9fcb2a54719433b745c
billow <yaagnto@qq.com>
2018-07-22 12:26:07 +0800

    complete 42

---
+++ basic/day_42/js/restaurant_es5.js
@@ -0,0 +1,84 @@
+function Restaurant(cash, seats, staff) {
+    if (typeof arguments[0] === 'object') {
+        let obj = arguments[0];
+        this.cash = obj.cash;
+        this.seats = obj.seats;
+        this.staff = obj.staff;
+    } else {
+        this.cash = cash;
+        this.seats = seats;
+        this.staff = staff;
+    }
+}
+
+Restaurant.prototype.hire = function (worker) {
+    this.staff.push(worker);
+};
+
+Restaurant.prototype.fire = function (worker) {
+    let i = this.staff.indexOf(worker);
+    if (i !== -1) {
+        this.staff.splice(i, 1);
+    }
+};
+
+function* IdMaker() {
+    let i = 0;
+    while (true) yield ++i;
+}
+
+let idMaker = IdMaker();
+
+function Worker(name, wage) {
+    this.id = idMaker.next().value;
+
+    if (typeof arguments[0] === 'object') {
+        let obj = arguments[0];
+        this.name = obj.name;
+        this.wage = obj.wage;
+    } else {
+        this.name = name;
+        this.wage = wage;
+    }
+}
+
+Worker.work = function () {
+    console.log('do some work');
+};
+
+function Waiter(name, wage) {
+    Worker.call(this, name, wage)
+}
+
+Waiter.work = function (args) {
+
+};
+
+function Cook(name, wage) {
+    Worker.call(this, name, wage);
+}
+
+Cook.work = function () {
+    console.log();
+};
+
+function Customer() {
+
+}
+
+Customer.orderDishes = function () {
+
+};
+Customer.eat = function () {
+
+};
+
+function Dishes(name, cost, price) {
+    this.name = name;
+    this.cost = cost;
+    this.price = price;
+}
+
+
+
+
---
+++ basic/day_42/js/restaurant_es6.js
@@ -0,0 +1,73 @@
+class Restaurant {
+    constructor() {
+        let obj = arguments[0];
+        this.cash = obj.cash;
+        this.seats = obj.seats;
+        this.staff = obj.staff;
+    }
+
+    hire(worker) {
+        this.staff.push(worker);
+    }
+
+    fire(worker) {
+        let i = this.staff.indexOf(worker);
+        if (i !== -1) {
+            this.staff.splice(i, 1);
+        }
+    }
+}
+
+function* idGen() {
+    let i = 0;
+    while (true) yield ++i;
+}
+
+class Worker {
+    constructor(name, wage) {
+        this.id = Worker.idGen.next().value;
+        this.name = name;
+        this.wage = wage;
+    }
+
+    work() {
+    }
+}
+
+Worker.idGen = idGen();
+
+class Waiter extends Worker {
+    constructor(name, wage) {
+        super(name, wage);
+    }
+
+    work() {
+        super.work();
+    }
+}
+
+class Cook extends Worker {
+    constructor(name, wage) {
+        super(name, wage);
+    }
+
+    work() {
+        super.work();
+    }
+}
+
+class Customer {
+    order() {
+    }
+
+    eat() {
+    }
+}
+
+class Dish {
+    constructor(name, cost, price) {
+        this.name = name;
+        this.cost = cost;
+        this.price = price;
+    }
+}
